caf2d9c22188373aaa34fa37a28457015989b5e7
Alperen Yildiz <alperenyildiz21@gmail.com>
2020-07-14 17:06:12 -0400

    finishing touches

--- src/app/weatherdisplay/weatherdisplay.component.ts
+++ src/app/weatherdisplay/weatherdisplay.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./weatherdisplay.component.css']
 })
 export class WeatherdisplayComponent implements OnInit {
+  tempInFarenheit = true;
   searchedCity = '';
   weatherData: object;
 
@@ -15,19 +16,39 @@ export class WeatherdisplayComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  toggleDegrees(): void{
+    if (this.tempInFarenheit === true) {
+      this.tempInFarenheit = false;
+    }
+    else {
+      this.tempInFarenheit = true;
+    }
+    console.log(this.tempInFarenheit);
+  }
+
   async getCityWeather(): Promise<any>{
     console.log(this.searchedCity);
     const url = `http://api.openweathermap.org/data/2.5/weather?q=${this.searchedCity}&units=imperial&appid=4d7ae3fa13615ebb6343600dbc80e952`;
     try{
       const response = await fetch(url, {mode: 'cors'});
       const weatherData = await response.json();
+      console.log(weatherData);
+      const weatherDescription = weatherData.weather[0].main;
+
+      const gifUrl = `http://api.giphy.com/v1/gifs/translate?api_key=1rWPIu8eWuuDUxz5BIeJh0BnEGjCTHsy&s=${weatherDescription}`;
+      const gifResponse = await fetch(gifUrl, {mode: 'cors'});
+      const gifData = await gifResponse.json();
+      const gif = gifData.data.images.original.url;
+
       const date = new Date();
       console.log(typeof(weatherData));
       const cityWeather = {
         cityName: weatherData.name,
         temp: {
           currTemp: weatherData.main.temp,
-          feelsLike: weatherData.main.feels_like
+          celsiusTemp: Number(this.farenheitToCelsius(weatherData.main.temp).toFixed(2)),
+          feelsLike: weatherData.main.feels_like,
+          celsiusFeelsLike: Number(this.farenheitToCelsius(weatherData.main.feels_like).toFixed(2))
         },
         dayAndTime: {
           day: this.dayOfWeekAsString(date.getDay()),
@@ -39,13 +60,14 @@ export class WeatherdisplayComponent implements OnInit {
         },
         wind: {
           windSpeed: weatherData.wind.speed,
-        }
+        },
+        weatherGif: gif,
       };
       console.log(cityWeather);
       this.weatherData = cityWeather;
     }
     catch (err){
-      console.log(err);
+      alert('City not found.');
     }
 
   }
@@ -53,5 +75,9 @@ export class WeatherdisplayComponent implements OnInit {
   dayOfWeekAsString(dayIndex: number): string {
     const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     return weekDays[dayIndex];
+  }
+
+  farenheitToCelsius(farenheit: number): number {
+    return ((farenheit - 32) * 5 / 9);
   }
 }
